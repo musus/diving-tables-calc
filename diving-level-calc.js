a2f8d788dd97b2f1143689a346cf5c7e9ff62859
Susumu Seino <seino@susu.mu>
2018-07-09 10:35:43 +0900

    Make diving level calc

---
+++ diving-level-calc.js
@@ -0,0 +1,326 @@
+function diveLevelCalc() {
+    const metre = document.getElementById('metre').value;
+    const time = document.getElementById('time').value;
+    // const divingArr = [metre, time];
+
+    if (metre <= 12) {
+        if (time <= 5) {
+            var level = "A";
+        }
+        if (time > 5 && time <= 15) {
+            var level = "B";
+        }
+        if (time > 16 && time <= 25) {
+            var level = "C";
+        }
+        if (time > 26 && time <= 30) {
+            var level = "D";
+        }
+        if (time > 31 && time <= 40) {
+            var level = "E";
+        }
+        if (time > 41 && time <= 50) {
+            var level = "F";
+        }
+        if (time > 51 && time <= 70) {
+            var level = "G";
+        }
+        if (time > 71 && time <= 80) {
+            var level = "H";
+        }
+        if (time > 81 && time <= 100) {
+            var level = "I";
+        }
+        if (time > 101 && time <= 110) {
+            var level = "J";
+        }
+        if (time > 111 && time <= 130) {
+            var level = "K";
+        }
+        if (time > 131 && time <= 150) {
+            var level = "L";
+        }
+    }
+
+    if (metre <= 15 && metre > 13) {
+        if (time <= 10) {
+            var level = "B";
+        }
+        if (time > 11 && time <= 15) {
+            var level = "C";
+        }
+        if (time > 16 && time <= 25) {
+            var level = "D";
+        }
+        if (time > 26 && time <= 30) {
+            var level = "E";
+        }
+        if (time > 31 && time <= 40) {
+            var level = "F";
+        }
+        if (time > 41 && time <= 50) {
+            var level = "G";
+        }
+        if (time > 51 && time <= 60) {
+            var level = "H";
+        }
+        if (time > 61 && time <= 70) {
+            var level = "I";
+        }
+        if (time > 71 && time <= 80) {
+            var level = "J";
+        }
+    }
+
+    if (metre <= 18 && metre > 16) {
+        if (time <= 10) {
+            var level = "B";
+        }
+        if (time > 11 && time <= 15) {
+            var level = "C";
+        }
+        if (time > 16 && time <= 20) {
+            var level = "D";
+        }
+        if (time > 21 && time <= 25) {
+            var level = "E";
+        }
+        if (time > 26 && time <= 30) {
+            var level = "F";
+        }
+        if (time > 31 && time <= 40) {
+            var level = "G";
+        }
+        if (time > 41 && time <= 50) {
+            var level = "H";
+        }
+        if (time > 51 && time <= 55) {
+            var level = "I";
+        }
+        if (time > 56 && time <= 60) {
+            var level = "J";
+        }
+        if (time > 61 && time <= 80) {
+            var level = "L";
+        }
+    }
+
+    if (metre <= 21 && metre > 19) {
+        if (time <= 5) {
+            var level = "B";
+        }
+        if (time > 6 && time <= 10) {
+            var level = "C";
+        }
+        if (time > 11 && time <= 15) {
+            var level = "D";
+        }
+        if (time > 16 && time <= 20) {
+            var level = "E";
+        }
+        if (time > 21 && time <= 30) {
+            var level = "F";
+        }
+        if (time > 31 && time <= 35) {
+            var level = "G";
+        }
+        if (time > 36 && time <= 40) {
+            var level = "H";
+        }
+        if (time > 41 && time <= 45) {
+            var level = "I";
+        }
+        if (time > 46 && time <= 50) {
+            var level = "J";
+        }
+        if (time > 51 && time <= 70) {
+            var level = "L";
+        }
+    }
+
+    if (metre <= 21 && metre > 19) {
+        if (time <= 5) {
+            var level = "B";
+        }
+        if (time > 6 && time <= 10) {
+            var level = "C";
+        }
+        if (time > 11 && time <= 15) {
+            var level = "D";
+        }
+        if (time > 16 && time <= 20) {
+            var level = "E";
+        }
+        if (time > 21 && time <= 30) {
+            var level = "F";
+        }
+        if (time > 31 && time <= 35) {
+            var level = "G";
+        }
+        if (time > 36 && time <= 40) {
+            var level = "H";
+        }
+        if (time > 41 && time <= 45) {
+            var level = "I";
+        }
+        if (time > 46 && time <= 50) {
+            var level = "J";
+        }
+        if (time > 51 && time <= 60) {
+            var level = "K";
+        }
+        if (time > 61 && time <= 70) {
+            var level = "L";
+        }
+    }
+
+    if (metre <= 24 && metre > 22) {
+        if (time <= 5) {
+            var level = "B";
+        }
+        if (time > 6 && time <= 10) {
+            var level = "C";
+        }
+        if (time > 11 && time <= 15) {
+            var level = "D";
+        }
+        if (time > 16 && time <= 20) {
+            var level = "E";
+        }
+        if (time > 21 && time <= 25) {
+            var level = "F";
+        }
+        if (time > 26 && time <= 30) {
+            var level = "G";
+        }
+        if (time > 31 && time <= 35) {
+            var level = "H";
+        }
+        if (time > 36 && time <= 40) {
+            var level = "I";
+        }
+        if (time > 41 && time <= 50) {
+            var level = "K";
+        }
+        if (time > 51 && time <= 60) {
+            var level = "L";
+        }
+    }
+
+    if (metre <= 27 && metre > 25) {
+        if (time <= 5) {
+            var level = "B";
+        }
+        if (time > 6 && time <= 10) {
+            var level = "C";
+        }
+        if (time > 11 && time <= 12) {
+            var level = "D";
+        }
+        if (time > 13 && time <= 15) {
+            var level = "E";
+        }
+        if (time > 16 && time <= 20) {
+            var level = "F";
+        }
+        if (time > 21 && time <= 25) {
+            var level = "G";
+        }
+        if (time > 26 && time <= 30) {
+            var level = "H";
+        }
+        if (time > 31 && time <= 40) {
+            var level = "J";
+        }
+        if (time > 41 && time <= 50) {
+            var level = "L";
+        }
+    }
+
+    if (metre <= 30 && metre > 28) {
+        if (time <= 5) {
+            var level = "B";
+        }
+        if (time > 6 && time <= 7) {
+            var level = "C";
+        }
+        if (time > 8 && time <= 10) {
+            var level = "D";
+        }
+        if (time > 11 && time <= 15) {
+            var level = "E";
+        }
+        if (time > 16 && time <= 20) {
+            var level = "F";
+        }
+        if (time > 21 && time <= 22) {
+            var level = "G";
+        }
+        if (time > 23 && time <= 25) {
+            var level = "H";
+        }
+        if (time > 31 && time <= 40) {
+            var level = "K";
+        }
+    }
+
+    if (metre <= 33 && metre > 31) {
+        if (time <= 5) {
+            var level = "C";
+        }
+        if (time > 6 && time <= 10) {
+            var level = "D";
+        }
+        if (time > 11 && time <= 13) {
+            var level = "E";
+        }
+        if (time > 14 && time <= 15) {
+            var level = "F";
+        }
+        if (time > 16 && time <= 20) {
+            var level = "G";
+        }
+        if (time > 21 && time <= 22) {
+            var level = "H";
+        }
+        if (time > 23 && time <= 30) {
+            var level = "K";
+        }
+    }
+
+    if (metre <= 36 && metre > 34) {
+        if (time <= 5) {
+            var level = "C";
+        }
+        if (time > 6 && time <= 10) {
+            var level = "D";
+        }
+        if (time > 11 && time <= 12) {
+            var level = "E";
+        }
+        if (time > 13 && time <= 25) {
+            var level = "I";
+        }
+        if (time > 26 && time <= 30) {
+            var level = "J";
+        }
+    }
+
+    if (metre <= 36 && metre > 34) {
+        if (time <= 5) {
+            var level = "C";
+        }
+        if (time > 6 && time <= 8) {
+            var level = "D";
+        }
+        if (time > 9 && time <= 10) {
+            var level = "E";
+        }
+        if (time > 11 && time <= 25) {
+            var level = "J";
+        }
+    }
+
+    document.getElementById('level').innerHTML = level;
+
+}
